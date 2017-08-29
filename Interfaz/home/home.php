eed939e36bafcc9976c2cdfcb417058f76875006
ManuelGMaestri <manuelgmaestri98@gmail.com>
2017-08-29 15:39:38 -0300

    home

---
+++ Interfaz/home/home.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HalfControl - Home</title>
+  <link href="../css_visuals/GSSheet.css" rel="stylesheet" type="text/css">
+  <link href="../css_visuals/W3Sheet.css" rel="stylesheet" type="text/css">
+  <link href="../css_visuals/demo.css" rel="stylesheet" type="text/css">
+  <link href="../css_visuals/footer.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Ubuntu+Condensed" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+</head>
+<body>
+ <div class="heading">Half<p class="headingLog">Control</p></div>
+ <div class="w3-row-padding w3-padding-64 w3-container">
+  <div class="w3-content">
+    <div class="w3-twothird">
+      <h1>Bienvenido a HalfControl</h1>
+      <h5 class="w3-padding-32">Este es el menu principal de HalfControl, aqui vas a poder elegir que controlar con tu sistema de automatización. Solo tenes que hacer click en EDITAR si queres configurar alguna tarea de tu controlador y MIRAR si queres ver el estado de los sensores.
+      </h5> 
+    </div>
+    <div class="w3-third w3-center homeIconSize">
+      <i class="fa fa-home anchorSize w3-padding-64 w3-text-red" style="font-size: 200px;"></i>
+    </div>
+  </div>
+</div>
+  <div class="w3-row">
+  <a href="mirar/index.php">
+   <div class="w3-hover-grayscale w3-display-container w3-half w3-container w3-green block">
+     <h2 class="w3-display-middle">MIRAR</h2> 
+      </div>
+  </a>
+  <a href="editar/index.php">
+    <div class="w3-hover-grayscale w3-display-container w3-half w3-blue w3-container block">
+     <h2 class="w3-display-middle">EDITAR</h2> 
+      </div>
+  </a>
+</div>
+    <footer class="footer-distributed">
+      <div class="footer-left">
+        <h3>Half<span class="">Control</span></h3>
+        <p class="footer-links">
+          <a href="#">Home</a>
+          ·
+          <a href="#">Sobre nosotros</a>
+          ·
+          <a href="#">Contacto</a>
+        </p>
+        <p class="footer-company-name">HalfServices &copy; 2016 - 2017</p>
+      </div>
+      <div class="footer-center">
+       <div>
+          <i class="fa fa-map-marker"></i>
+          <p><span></span> Tandil, Argentina</p>
+        </div>
+        <div>
+          <i class="fa fa-phone"></i>
+          <p>+54 2494660399</p>
+        </div>
+        <div>
+          <i class="fa fa-envelope"></i>
+          <p><a href="mailto:half.web.services@gmail.com">Half.Services@gmail.com</a></p>
+        </div>
+      </div>
+      <div class="footer-right">
+        <p class="footer-company-about">
+          <span>Sobre la empresa</span>
+          Impulsados por los nuevos avances en la informatica, dia a dia innovamos con
+          nuevos proyectos y desarrollos orientados a mejorar la calidad de vida.
+        </p>
+        <div class="footer-icons">
+          <a href="#"><i class="fa fa-facebook"></i></a>
+          <a href="#"><i class="fa fa-twitter"></i></a>
+          <a href="#"><i class="fa fa-linkedin"></i></a>
+          <a href="#"><i class="fa fa-github"></i></a>
+        </div>
+      </div>
+    </footer>
+</body>
+</html>
