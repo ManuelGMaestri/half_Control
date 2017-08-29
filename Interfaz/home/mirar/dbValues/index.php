451cc734b1837f4f4005df1ceff9585a9b624b28
ManuelGMaestri <manuelgmaestri98@gmail.com>
2017-08-29 15:47:45 -0300

    dbValues_index

---
+++ Interfaz/home/mirar/dbValues/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title>HalfControl - Base de datos - Mirar</title>
+ <link href="../../../css_visuals/GSSheet.css" rel="stylesheet" type="text/css">
+ <link href="../../../css_visuals/W3Sheet.css" rel="stylesheet" type="text/css">
+ <link href="../../../css_visuals/demo.css" rel="stylesheet" type="text/css">
+ <link href="../../../css_visuals/footer.css" rel="stylesheet" type="text/css">
+ <link href="https://fonts.googleapis.com/css?family=Montserrat|Ubuntu+Condensed" rel="stylesheet">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+</head>
+<body ondragstart="return false" onselectstart="return false" oncontextmenu="return false">
+<a href="../home.php"><div class="heading"> Half<p class="headingLog">Control</p></div></a>
+<!--footer-->
+ <footer class="footer-distributed">
+      <div class="footer-left">
+        <h3>Half<span class="">Control</span></h3>
+        <p class="footer-links">
+          <a href="#">Home</a>
+          ·
+          <a href="#">Sobre nosotros</a>
+          ·
+          <a href="#">Contacto</a>
+        </p>
+        <p class="footer-company-name">HalfServices &copy; 2016 - 2017</p>
+      </div>
+      <div class="footer-center">
+       <div>
+          <i class="fa fa-map-marker"></i>
+          <p><span></span> Tandil, Argentina</p>
+        </div>
+        <div>
+          <i class="fa fa-phone"></i>
+          <p>+54 2494 660399</p>
+        </div>
+        <div>
+          <i class="fa fa-envelope"></i>
+          <p><a href="mailto:half.web.services@gmail.com">Half.Services@gmail.com</a></p>
+        </div>
+      </div>
+      <div class="footer-right">
+        <p class="footer-company-about">
+          <span>Sobre la empresa</span>
+          Impulsados por los nuevos avances en la informatica, dia a dia innovamos con
+          nuevos proyectos y desarrollos orientados a mejorar la calidad de vida.
+        </p>
+        <div class="footer-icons">
+          <a href="#"><i class="fa fa-facebook"></i></a>
+          <a href="#"><i class="fa fa-twitter"></i></a>
+          <a href="#"><i class="fa fa-linkedin"></i></a>
+          <a href="#"><i class="fa fa-github"></i></a>
+         </div>
+        </div>
+       </footer>
+       <script src="../../js/lib/Chart.min.js"></script>
+      <script src="../../js/grafoTemp.js"></script>
+     <script src="../../js/grafoSysTemp.js"></script>
+    <script src="../../js/converSens.js"></script>
+   <script src="../../js/estadoActu.js"></script>
+  </body>
+</html>
